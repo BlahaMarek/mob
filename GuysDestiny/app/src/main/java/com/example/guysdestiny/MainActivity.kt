b0c3375f1518a5485b444c43a6e4985285babfa7
karllos50 <karol.trabalka@gmail.com>
2019-11-27 13:45:41 +0100

    merge conflict chat > post > master

--- GuysDestiny/app/src/main/java/com/example/guysdestiny/MainActivity.kt
+++ GuysDestiny/app/src/main/java/com/example/guysdestiny/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.guysdestiny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import android.provider.MediaStore
 import com.giphy.sdk.core.models.Media
 import com.giphy.sdk.ui.GPHSettings
@@ -10,10 +9,8 @@ import com.giphy.sdk.ui.GiphyCoreUI
 import com.giphy.sdk.ui.themes.GridType
 import com.giphy.sdk.ui.themes.LightTheme
 import com.giphy.sdk.ui.views.GiphyDialogFragment
-=======
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
->>>>>>> bcb954c38492b5e5dc1363487d139ab1423a4463
 
 class MainActivity : AppCompatActivity() {
 
